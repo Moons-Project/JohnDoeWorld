4c1d5fd059a1bcf760f9915ca4ae969d22c032e0
DaddyTrap <914670787@qq.com>
2018-02-06 14:19:04 +0800

    :sparkles: 加入检测梯子的方法

---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SimpleMove : MonoBehaviour {
+
+	private GameObject controlObject;
+	private TestSceneManager manager;
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start() {
+		controlObject = this.gameObject;
+		manager = TestSceneManager.instance;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector2 prevPos = controlObject.transform.position;
+		if (Input.GetKey(KeyCode.W)) {
+			var ret = manager.HasLadder(gameObject, TestSceneManager.Direction.Up);
+			if (ret.Count != 0)
+				Debug.Log(ret[0]);
+			else
+				Debug.Log("No ladders.");
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			var ret = manager.HasLadder(gameObject, TestSceneManager.Direction.Down);
+			if (ret.Count != 0)
+				Debug.Log(ret[0]);
+			else
+				Debug.Log("No ladders.");
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			prevPos.x -= speed;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			prevPos.x += speed;
+		}
+		controlObject.transform.position = prevPos;
+
+	}
+
+	void OnMouseDown() {
+		Debug.Log("hhh");
+	}
+}
---
+++ Assets/Scripts/TestSceneManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestSceneManager : MonoBehaviour {
+
+	public Tilemap ladderTilemap;
+	public Tilemap platformTilemap;
+
+	private TilemapCollider2D ladderCollider;
+	private TilemapCollider2D platformCollider;
+	
+	public static TestSceneManager instance;
+
+
+	public static readonly float CHECKING_DELTA = 0.3f;
+	public enum Direction {
+		Up,
+		Down,
+		Left,
+		Right,
+		All
+	}
+
+	void Awake() {
+		if (instance == null)
+			instance = this;
+	}
+
+	// Use this for initialization
+	void Start() {
+		ladderCollider = ladderTilemap.gameObject.GetComponent<TilemapCollider2D>();
+		platformCollider = platformTilemap.gameObject.GetComponent<TilemapCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public List<Vector3Int> HasLadder(GameObject gObj, Direction direction) {
+		List<Vector3Int> posList = new List<Vector3Int>();
+
+		Collider2D collider = gObj.GetComponent<Collider2D>();
+
+		if (collider == null) return posList;
+		Bounds bounds = ladderCollider.bounds;
+		Vector3 centerPoint = collider.bounds.center;
+
+		if (direction == Direction.Up) {
+			/* 
+			检测上方梯子
+			直接使用gObj的中心点，检测其是否在梯子中
+			 */
+			Vector3 vec3 = centerPoint;
+			Vector3Int cellPos = ladderTilemap.WorldToCell(vec3);
+			bool collision = ladderTilemap.GetTile(cellPos) != null;
+			if (collision) {
+				Vector3Int temp = ladderTilemap.WorldToCell(vec3);
+				posList.Add(temp);
+			}
+		} else if (direction == Direction.Down) {
+			/* 
+			检测下方梯子
+			使用gObj的下边界中心，检测其是否在梯子中
+			 */
+			Vector3 vec3 = centerPoint;
+			vec3.y -= bounds.extents.y + CHECKING_DELTA;
+			Vector3Int cellPos = ladderTilemap.WorldToCell(vec3);
+			bool collision = ladderTilemap.GetTile(cellPos) != null;
+			if (collision) {
+				Vector3Int temp = ladderTilemap.WorldToCell(vec3);
+				posList.Add(temp);
+			}
+		} else {
+			return posList;
+		}
+
+		return posList;
+	}
+}
