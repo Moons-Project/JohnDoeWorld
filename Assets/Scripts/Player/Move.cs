1bbaa11eb0b9b61eee60900ed2cf12f64a9c405c
jskyzero <jskyzero@outlook.com>
2018-02-12 05:19:44 +0800

    :art: add basic move script

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+  public float maxVelocityX= 10f;
+  public float maxVelocityY = 5f;
+
+
+
+  private Rigidbody2D body;
+  private Animator animator;
+
+
+  private bool isFacingRight;
+
+  // Use this for initialization
+  void Start() {
+    body = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+  }
+
+  void FixedUpdate() {
+    float horizontalAxis = Input.GetAxis("Horizontal");
+    bool jumpButtonDown = Input.GetButtonDown("Jump");
+
+    UpdateFacing(horizontalAxis);
+
+    float velocityX = horizontalAxis * maxVelocityX;
+    float velocityY = CanJump(jumpButtonDown) ? maxVelocityY : body.velocity.y;
+    animator.SetFloat("velocityX", Mathf.Abs(body.velocity.x));
+    animator.SetFloat("velocityY", Mathf.Abs(body.velocity.y));
+    body.velocity = new Vector2(velocityX, velocityY);
+  }
+
+  bool CanJump(bool jumpButtonDown) {
+    bool isGround = true;
+    return jumpButtonDown && isGround;
+  }
+
+  void UpdateFacing(float horizontalAxis) {
+    if (horizontalAxis > 0 && !isFacingRight)
+      Flip();
+    else if (horizontalAxis < 0 && isFacingRight)
+      Flip();
+  }
+
+  void Flip() {
+    // 改变朝向
+    isFacingRight = !isFacingRight;
+    // 改变x方向上的scale进行翻转
+    transform.localScale =  Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+  }
+}
