8ce42a8130f276b7b450420cfe804ad0e91c9799
jskyzero <jskyzero@outlook.com>
2018-02-27 16:14:15 +0800

    :pill: 添加字体

--- Assets/Resources/Scripts/VirtualDoorControl.cs
+++ Assets/Resources/Scripts/VirtualDoorControl.cs
@@ -69,6 +69,9 @@ public class VirtualDoorControl : MonoBehaviour {
         case ScriptType.PleaseGoNextWay:
           PleaseGoNextWay();
           break;
+        case ScriptType.ChangeToSene5ToJane:
+          ChangeToSene5ToJane();
+          break;
       }
     }
   }
