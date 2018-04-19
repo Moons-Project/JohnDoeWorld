d91477d1d476567af1c5461f82a22ad97c22e3dc
DaddyTrap <914670787@qq.com>
2018-04-19 16:28:24 +0800

    :construction: 完成拉杆机关(未测试)

---
+++ Assets/Resources/Scripts/LeverTriggerBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LeverTriggerBehavior : MonoBehaviour {
+
+  public Tilemap LeftLadderTilemap, RightLadderTilemap;
+  public string SoundEffect;
+  public Sprite spriteToChange;
+
+  // Use this for initialization
+  void Start () {
+    
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    
+  }
+
+  void OnTriggerEnter2D(Collider2D collider) {
+    if (collider.CompareTag("ControlPlayer")) {
+      if (SoundEffect != null) GameManager.instance.musicManager.PlaySE(SoundEffect);
+      LeftLadderTilemap.gameObject.SetActive(false);
+      RightLadderTilemap.gameObject.SetActive(true);
+      GameManager.instance.tilemapManager.ladderTilemap = RightLadderTilemap;
+      GetComponent<SpriteRenderer>().sprite = spriteToChange;
+    }
+  }
+}
